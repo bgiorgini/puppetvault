522f467619f4ab47e1fd9eb7a65ac4aadb1998cc
Andres Villarroel <andres.via@gmail.com>
2014-05-15 16:58:30 -0500

    Some minor changes on the post_install.sh script.

--- debian_preseed/post_install.sh
+++ debian_preseed/post_install.sh
@@ -5,11 +5,11 @@
 PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 export PATH
 
-newhn=$(uuidgen)
-oldhn=$(hostname)
+newhn="$(uuidgen)"
+oldhn="$(hostname -s)"
 
 hostname $newhn
-echo $newhn > /etc/hostname
-sed -i 's|\(\s\)'$oldhn'$|\1'$newhn'|g' /etc/hosts
+echo "$newhn" > /etc/hostname
+sed -i 's|\(\s\)'"$oldhn"'[^a-zA-Z0-9]|\1'"$newhn"'|g' /etc/hosts
 sed -i 's|^START=no$|START=yes|' /etc/default/puppet
 
