936438aad6c58e857ce4f0b3371075e000a1964d
root <root@frodo-meriadoc-klingon.(none)>
2014-05-16 17:32:16 -0500

    Definition of new site.

---
+++ manifests/site.pp
@@ -0,0 +1,23 @@
+
+# main site definition
+
+node  'frodo-meriadoc-klingon'
+{
+  # puppet
+}
+
+node  'freon-momento-stark'
+{
+  # web
+}
+
+node  'guarismo-aragorn-stegosaurus'
+{
+  # db
+}
+
+node  'scalar-gollum-reindeer'
+{
+  # mail
+}
+
