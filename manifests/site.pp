d6a2ac2429567f0c799d3cfda151f6610f725bc5
root <root@frodo-meriadoc-klingon.(none)>
2014-05-19 10:56:41 -0500

    Last change before starting to use Vagrant.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,23 +1,58 @@
 
 # main site definition
 
-node  'frodo-meriadoc-klingon'
+$puppetnode = 'frodo-meriadoc-klingon'
+
+node 'frodo-meriadoc-klingon'
 {
   # puppet
 }
 
-node  'freon-momento-stark'
+node 'freon-momento-stark'
 {
   # web
 }
 
-node  'guarismo-aragorn-stegosaurus'
+node 'guarismo-aragorn-stegosaurus'
 {
   # db
 }
 
-node  'scalar-gollum-reindeer'
+node 'scalar-gollum-reindeer'
 {
   # mail
 }
+
+class ssh::params {
+  case $::osfamily {
+  'Debian': { $sshd_package  = 'ssh' }
+  'RedHat': { $sshd_package  = 'openssh-server' }
+  default:  {fail("Login class does not work on osfamily: ${::osfamily}")}
+  }
+}
+
+class ssh inherits ssh::params {
+  package { $::ssh::params::sshd_package:
+    ensure => installed,
+  }
+}
+
+include ssh
+
+# class ssh::params {
+#   case $::osfamily {
+#   'Debian': { $sshd_package  = 'ssh' }
+#   'RedHat': { $sshd_package  = 'openssh-server' }
+#   default:  {fail("Login class does not work on osfamily: ${::osfamily}")}
+#   }
+# }
+# 
+# class ssh {
+#   include ssh::params
+#   package { $::ssh::params::sshd_package:
+#     ensure => installed,
+#   }
+# }
+# 
+# include ssh
 
